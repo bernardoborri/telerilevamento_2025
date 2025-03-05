f12158abe8ed0121da49516a2d24e318213185da
bernardoborri <borri.pachypus@gmail.com>
2025-03-05 19:50:57 +0100

    Create 02_visualization.R

---
+++ code/02_visualization.R
@@ -0,0 +1,30 @@
+# R code for visualizing satellite data
+
+# install.packages("devtools")
+library(devtools)
+install_github("ducciorocchini/imageRy")
+
+library(terra)
+library(imageRy)
+
+im.list()
+ 
+# For the whole course we are going to make use of = instead of <-
+# This is based on the following video:
+# https://www.youtube.com/watch?v=OJMpKCKH1hM
+
+b2 = im.import("sentinel.dolomites.b2.tif")
+plot(b2, col=cl)
+
+cl = colorRampPalette(c("black", "dark grey", "light grey"))(100)
+plot(b2, col=cl)
+
+cl = colorRampPalette(c("black", "dark grey", "light grey"))(3)
+plot(b2, col=cl)
+# tlumley@u.washington.edu, Thomas Lumley
+
+# Exercise: make your own color ramp
+# https://sites.stat.columbia.edu/tzheng/files/Rcolor.pdf
+
+cl = colorRampPalette(c("royalblue3", "seagreen1", "red1"))(100)
+plot(b2, col=cl)
