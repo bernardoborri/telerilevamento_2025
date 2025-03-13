3621baa4f319fc3b2e2f8d8a391abbd1b91cb54d
bernardoborri <borri.pachypus@gmail.com>
2025-03-13 12:25:58 +0100

    03_spectral_indices.R

--- code/03_spectral_indices.R
+++ code/03_spectral_indices.R
@@ -20,14 +20,36 @@ mato2006 = im.import("matogrosso_ast_2006209_lrg.jpg)
 mato2006 = flip(mato2006)
 plot(mato2006)
 
+#uso mltiframe per mettere a confronto '92 e '06
 im.multiframe(1, 2)
 im.plotRGB(mato1992, r=2, g=3, b=1, title="Mato Grosso 1992")
 im.plotRGB(mato2006, r=2, g=3, b=1, title="Mato Grosso 2006")
 
-#radiometric resolution
-plot(mato1992 
+#variazioni nel numero di righe e colonne e nella scelta delle bande da rappresentare
 
+im.multiframe(3,2)
+# NIR ontop of red
+im.plotRGB(mato1992, r=1, g=2, b=3)
+im.plotRGB(mato2006, r=1, g=2, b=3)
+
+# NIR ontop of green
+im.plotRGB(mato1992, r=2, g=1, b=3)
+im.plotRGB(mato2006, r=2, g=1, b=3)
+
+# NIR ontop of blue
+im.plotRGB(mato1992, r=3, g=2, b=1)
+im.plotRGB(mato2006, r=3, g=2, b=1)
+
+# Exercise: plot only the first layer of mato2006
+dev.off()
+# scelgo i colori dal pacchetto viridis
+
+plot(mato2006[[1]])
+plot(mato2006[[1]], col=magma(100))
+plot(mato2006[[1]], col=mako(100))
 
 # DVI: Difference Vegetation Index
-#Tree:NIR=255, red=0, DVI=NIR-red=255
-#stressedtree: NIR=100, red=20, DVI=NIR-red=100-20=80
+#Tree
+NIR=255, red=0, DVI=NIR-red=255
+#stressedtree
+NIR=100, red=20, DVI=NIR-red=100-20=80
