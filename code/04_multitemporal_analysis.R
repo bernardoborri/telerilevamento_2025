7be0c278a6a8ea084e79432049dacf8a72e9e6f7
bernardoborri <borri.pachypus@gmail.com>
2025-03-20 14:25:50 +0100

    Update 04_multitemporal_analysis.R

--- code/04_multitemporal_analysis.R
+++ code/04_multitemporal_analysis.R
@@ -55,35 +55,6 @@ plot(grdif)
 #il path riporterà una scritta simile a C:\Users\bobby\OneDrive\Desktop
 #bisogna cambiare la direzione degli slash nel comando come segue
 
-#Exporting data
-setwd("C:/Users/bobby/OneDrive/Desktop")
-#a questo punto scrivendo
-getwd()
-#su R comparirà la scritta
-"C:/Users/bobby/OneDrive/Desktop"
-plot(gr)
-
-#esporto dunque l'immagine in png sul desktop secondo i comandi che seguono
-png("greenland_output.png")
-plot(gr)
-dev.off()
-
-#provo ad esportare ora la stessa immagine come file PDF
-pdf("greenland_output.png")
-plot(gr)
-dev.off()
-
-#esporto direttamente una differenza come file pdf
-pdf(difgreen.pdf")
-plot(grdif)
-dev.off()
-
-#esporto in formato jpeg
-jpeg("difgreen.jpeg")
-plot(grdif)
-dev.off()
-
-#il pdf non perde definizione zoomando
 
 
 
