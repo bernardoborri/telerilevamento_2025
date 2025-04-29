02f26de28c07ddf5490bcee9d27cf799423e1f87
bernardoborri <borri.pachypus@gmail.com>
2025-04-29 17:11:21 +0200

    Update 11_functions.R

--- code/11_functions.R
+++ code/11_functions.R
@@ -3,9 +3,9 @@
 # Di seguito un esempio
 
 somma <- function(x,y) {
-  z=x+y
-  return(z)
-  }
+z=x+y
+return(z)
+}
 # Le parentesi graffe contengono il corpo della funzione, cioè le istruzioni da eseguire 
 
 # Ora che ho creato la funzione, la provo con degli argomenti (numeri) scelti arbitrariamente
@@ -16,36 +16,40 @@ somma(3, 5)
 
 # Exercise: make a new function called differenza
 differenza <- function(x,y){
-  z=x-y
-  return(z)
-  }
+z=x-y
+return(z)
+}
 
 # Provo adesso a creare una funzione che faccia un multiframe
 mf <- function(nrow,ncol) {
-  par(mfrow=c(nrow,ncol))
-  }
+par(mfrow=c(nrow,ncol))
+}
 
 
- # Creo ora una funzione che mi riveli se un numero è positivo o meno e gli do nome “positivo”
+# Creo ora una funzione che mi riveli se un numero è positivo o meno e gli do nome “positivo”
 positivo <- function(x) {
-  if(x>0) {
-    print("Questo numero è positivo, non lo sai?")
-    }
-  # Qui sopra “x” è il numero che inserirò per capire se è positivo. 
-  # “if” è una funzione molto utilizzata e qui si dice che è “nested”, ovvero annidata nella nostra nuova funzione. 
-  # Altrettanto nested è qui sopra la funzione “print”, che fa comparire un messaggio: in questo caso qualora x sia positivo apparirà il messaggio "Questo numero è positivo, non lo sai?"
+if(x>0) {
+print("Questo numero è positivo, non lo sai?")
+}
+# Qui sopra “x” è il numero che inserirò per capire se è positivo. 
+# “if” è una funzione molto utilizzata e qui si dice che è “nested”, ovvero "annidata" nella nostra nuova funzione. 
+# Altrettanto nested è qui sopra la funzione “print”, che fa comparire un messaggio: in questo caso qualora "x" sia positivo apparirà il messaggio "Questo numero è positivo, non lo sai?"
   
-  else if(x<0) {
-    print("Questo numero è negativo, studia!") 
-    }
+else if(x<0) {
+print("Questo numero è negativo, studia!") 
+}
 # Qui sopra con l’ulteriore funzione nested “else if” definiamo che se x è minore di 0 la funzione nested “print” ci darà il messaggio "Questo numero è negativo, studia!"
 
-  # A questo punto, con i medesimi metodi e funzioni nested definiamo anche che cosa accadrà qualora il numero sia zero, ovvero che appaia il messaggio “lo zero è zero”  
-  else {
-    print("Lo zero è zero.")}
-  }
+# A questo punto, con i medesimi metodi e funzioni nested definiamo anche che cosa accadrà qualora il numero sia zero, ovvero che appaia il messaggio “lo zero è zero”  
+else {
+print("Lo zero è zero.")}
+}
 
+# Abbiamo visto che quando usiamo la funzione “im.import” per un’immagine, imageRy la fa comparire automaticamente
+# Invece con il comando “flip” è necessario poi aggiungere dopo il comando “plot” per far apparire l’immagine capovolta
+# Facciamo dunque una funzione di “flip intelligente” e la chiamiamo “flipint”:
 flipint <- function(x) {
-  x = flip(x)
-  plot(x)
-  }
+x = flip(x)
+plot(x)
+}
+# Questa funzione dice che “flipint”, presa l’immagine “x”, applicherà un flip di “x” e ne produrrà il plot in modo immediatamente consecutivo
